2bacd3e4426a8ca85155f520cd343d683eb47f32
donghyeon505 <ehdgus458@gmail.com>
2025-07-09 11:17:34 +0900

    feat: 올바른 숫자와 기호를 받을 수 있도록 반복문 추가
    -불필요한 주석 제거

--- src/main/java/lv1/Main.java
+++ src/main/java/lv1/Main.java
@@ -4,25 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello, Calculator!");
+
         // 숫자 입력
         Scanner scanner = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int n1 = scanner.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int n2 = scanner.nextInt();
+        // n1, n2 선언
+        int n1;
+        int n2;
 
-        // 숫자 확인 테스트용
-//        System.out.println("n1 = " + n1);
-//        System.out.println("n2 = " + n2);
+        // 0이상의 양의 정수만 받기
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            n1 = scanner.nextInt();
+            if (n1 >= 0) break;
+            System.out.println("0 이상의 정수를 입력하세요");
+        }
+        while (true) {
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            n2 = scanner.nextInt();
+            if (n2 >= 0) break;
+            System.out.println("0 이상의 정수를 입력하세요");
+        }
+
+        // oper 선언
+        char oper;
 
         // 사칙연산 기호 전달받기
-        System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
-        char oper = scanner.next().charAt(0);
+        while (true) {
+            System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
+            oper = scanner.next().charAt(0);
+            if (oper == '+' ||  oper == '-' || oper == '*' || oper == '/' ) break;
+            System.out.println("올바른 기호를 입력하세요.");
+        }
 
-        // 사칙연산 기호 확인 테스트용
-//        System.out.println("operation = " + oper);
-
-        // 연산 진행
+        // 결과값 0으로 초기화
         int result = 0;
 
         // 연산 구분하기
