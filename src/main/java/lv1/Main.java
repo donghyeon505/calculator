9acd1ba37edadd66f67b37c1bf0dd0e634e89d16
donghyeon505 <ehdgus458@gmail.com>
2025-07-09 11:52:41 +0900

    feat: 계산기 무한 반복 및 exit 입력 시 종료 기능 추가 -나눗셈 오류 시 결과값이 나오지않도록 수정 -반복문 추가에 따라 변수 선언 위치 조정

--- src/main/java/lv1/Main.java
+++ src/main/java/lv1/Main.java
@@ -5,53 +5,81 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Calculator!");
 
-        // 숫자 입력
         Scanner scanner = new Scanner(System.in);
-        // n1, n2 선언
+        // 변수들 선언
+        String replay;
         int n1;
         int n2;
 
-        // 0이상의 양의 정수만 받기
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            n1 = scanner.nextInt();
-            if (n1 >= 0) break;
-            System.out.println("0 이상의 정수를 입력하세요");
-        }
-        while (true) {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            n2 = scanner.nextInt();
-            if (n2 >= 0) break;
-            System.out.println("0 이상의 정수를 입력하세요");
-        }
+        System.out.println("Hello, Calculator!");
 
-        // oper 선언
-        char oper;
+        do {
+            // 숫자 입력
+            // n1, n2 선언
 
-        // 사칙연산 기호 전달받기
-        while (true) {
-            System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
-            oper = scanner.next().charAt(0);
-            if (oper == '+' ||  oper == '-' || oper == '*' || oper == '/' ) break;
-            System.out.println("올바른 기호를 입력하세요.");
-        }
+            // 0이상의 양의 정수만 받기
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                n1 = scanner.nextInt();
+                if (n1 >= 0) break;
+                System.out.println("0 이상의 정수를 입력하세요");
+            }
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                n2 = scanner.nextInt();
+                if (n2 >= 0) break;
+                System.out.println("0 이상의 정수를 입력하세요");
+            }
 
-        // 결과값 0으로 초기화
-        int result = 0;
+            // oper 선언
+            char oper;
 
-        // 연산 구분하기
-        switch (oper) {
-            case '+': result = n1 + n2; break;
-            case '-': result = n1 - n2; break;
-            case '*': result = n1 * n2; break;
-            case '/': if (n2 == 0) {
-                System.out.println("나눗셈 연산에서는 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-            } else {result = n1 / n2;} break;
-        }
+            // 사칙연산 기호 전달받기
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
+                oper = scanner.next().charAt(0);
+                if (oper == '+' || oper == '-' || oper == '*' || oper == '/') break;
+                System.out.println("올바른 기호를 입력하세요.");
+            }
 
-        //결과값 입력
-        System.out.println("result = " + result);
+            // 변수들 초기화
+            int result = 0;
+            boolean letResult = true;
+
+            // 연산 구분하기
+            switch (oper) {
+                case '+':
+                    result = n1 + n2;
+                    break;
+                case '-':
+                    result = n1 - n2;
+                    break;
+                case '*':
+                    result = n1 * n2;
+                    break;
+                case '/':
+                    if (n2 == 0) {
+                        System.out.println("나눗셈 연산에서는 분모(두번째 숫자)에 0이 입력될 수 없습니다.");
+                        letResult = false;
+                    } else {
+                        result = n1 / n2;
+                    }
+                    break;
+            }
+
+            //결과값 입력
+            if (letResult) {
+                System.out.println("result = " + result);
+            }
+
+            // 반복 여부
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료):");
+            replay = scanner.next();
+
+        } while (!replay.equals("exit"));
+
+        //계산기 종료
+        System.out.print("계산기 종료");
     }
 }
