105900d807a1606c57473081cf406334d23bb6b5
donghyeon505 <ehdgus458@gmail.com>
2025-07-09 16:42:09 +0900

    feat: 계산기 무한 반복 추가 및 숫자 입력 오류방지 추가

--- src/main/java/lv2/Main.java
+++ src/main/java/lv2/Main.java
@@ -6,37 +6,64 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // 인스턴스 화
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
+        // 변수 선언
+        String replay;
         int n1;
         int n2;
         char oper;
 
-        // 0이상의 양의 정수만 받기
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            n1 = scanner.nextInt();
-            if (n1 >= 0) break;
-            System.out.println("0 이상의 정수를 입력하세요");
-        }
-        while (true) {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            n2 = scanner.nextInt();
-            if (n2 >= 0) break;
-            System.out.println("0 이상의 정수를 입력하세요");
-        }
+        // 시작
+        System.out.println("Hello, Calculator!");
 
-        // 사칙연산 기호 전달받기
-        while (true) {
-            System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
-            oper = scanner.next().charAt(0);
-            if (oper == '+' || oper == '-' || oper == '*' || oper == '/') break;
-            System.out.println("올바른 기호를 입력하세요.");
-        }
+        // 반복
+        do {
+            // 0이상의 양의 정수만 받기 + 다른 타입의 입력시 오류 처리
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                if (scanner.hasNextInt()) {
+                    n1 = scanner.nextInt();
+                    if (n1 >= 0) break;
+                    System.out.println("0 이상의 정수를 입력하세요");
+                } else {
+                    System.out.println("숫자만 입력해주세요.");
+                    scanner.next();
+                }
+            }
 
-        int result = calculator.calcul(n1, n2, oper);
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                if (scanner.hasNextInt()) {
+                    n2 = scanner.nextInt();
+                    if (n2 >= 0) break;
+                    System.out.println("0 이상의 정수를 입력하세요");
+                } else {
+                    System.out.println("숫자만 입력해주세요.");
+                    scanner.next();
+                }
+            }
+
+            // 사칙연산 기호 전달받기
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요(+,-,*,/): ");
+                oper = scanner.next().charAt(0);
+                if (oper == '+' || oper == '-' || oper == '*' || oper == '/') break;
+                System.out.println("올바른 기호를 입력하세요.");
+            }
+
+            int result = calculator.calcul(n1, n2, oper);
             System.out.println("result = " + result);
 
+            // 반복 여부
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료):");
+            replay = scanner.next();
+
+        } while (!replay.equals("exit"));
+
+        //계산기 종료
+        System.out.print("계산기 종료");
     }
 }
