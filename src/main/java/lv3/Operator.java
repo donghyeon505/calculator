06d37b312a0c418a0d871bd87fbd57d17303f712
donghyeon505 <ehdgus458@gmail.com>
2025-07-10 13:23:13 +0900

    feat: 문자 연산 기호를 enum으로 변환하는 fromChar 메서드 추가

--- src/main/java/lv3/Operator.java
+++ src/main/java/lv3/Operator.java
@@ -1,5 +1,19 @@
 package lv3;
 
 public enum Operator {
-    ADD, SUB, MUL, DIV
+    ADD('+'), SUB('-'), MUL('*'), DIV('/');
+
+    private final char oper;
+    Operator(char oper) {
+        this.oper = oper;
+    }
+
+    public static Operator fromChar(char ch) {
+        for (Operator op : Operator.values()) {
+            if (op.oper == ch) {
+                return op;
+            }
+        }
+        return null;
+    }
 }
