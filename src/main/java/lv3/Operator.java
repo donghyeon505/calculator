1727d27d1b4b397d4e680bb27c7b3306d498248f
donghyeon505 <ehdgus458@gmail.com>
2025-07-10 15:02:26 +0900

    주석 추가

--- src/main/java/lv3/Operator.java
+++ src/main/java/lv3/Operator.java
@@ -3,11 +3,15 @@ package lv3;
 public enum Operator {
     ADD('+'), SUB('-'), MUL('*'), DIV('/');
 
+    // oper 저장하는 필드
     private final char oper;
+
+    // oper 연산자 초기화
     Operator(char oper) {
         this.oper = oper;
     }
 
+    // 받은 문자에 대응하는 Operator enum을 반환
     public static Operator fromChar(char ch) {
         for (Operator op : Operator.values()) {
             if (op.oper == ch) {
