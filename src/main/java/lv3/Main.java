1f18751530108d17ec298772eee90d45ca3b148d
donghyeon505 <ehdgus458@gmail.com>
2025-07-11 20:04:10 +0900

    feat: exit 외에 기능들을 다양하게 추가 - 이 기능들을 모듈화 할 예정

--- src/main/java/lv3/Main.java
+++ src/main/java/lv3/Main.java
@@ -22,7 +22,10 @@ public class Main {
             // 변수 선언
             double n1;
             double n2;
+            double n3;
             Operator operEnum;
+            String view;
+            String delete;
 
             // 모든 숫자 받기 가능 + 다른 타입(문자) 입력시 다시 입력
             while (true) {
@@ -66,22 +69,125 @@ public class Main {
             // 결과값
             double result = calculator.calculate(n1, n2, operEnum);
             System.out.println("result: " + result);
+            List<Double> viewList = calculator.getResultList();
 
-            // 반복 여부
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료):");
-            replay = scanner.next();
+            // 기능 묻기
+            System.out.println("1. 결과값 조회하기(view)");
+            System.out.println("2. 결과값 초기화하기(delete)");
+            System.out.println("3. 계속 계산하기(cont)");
+            System.out.println("4. 계산기 종료하기(exit)");
+
+            // 기능 실행 반복문
+            label:
+            while (true) {
+                System.out.print("원하시는 기능을 입력해주세요: ");
+                replay = scanner.next();
+
+                switch (replay) {
+                    // 조회
+                    case "view":
+                        System.out.println("조회기능 목록");
+                        System.out.println("1. 입력값보다 낮은 수");
+                        System.out.println("2. 입력값보다 높은 수");
+                        System.out.println("3. 전체 조회");
+                        System.out.println("4. 취소");
+                        viewlabel:
+                        while (true) {
+                            System.out.print("원하시는 기능의 번호를 입력해주세요: ");
+                            view = scanner.next();
+                            switch (view) {
+                                case "1":
+                                    System.out.print("해당 값을 입력해주세요: ");
+                                    n3 = scanner.nextDouble();
+                                    System.out.println("result = " + calculator.resultSearch2(n3));
+                                    break viewlabel;
+                                case "2":
+                                    System.out.print("해당 값을 입력해주세요: ");
+                                    n3 = scanner.nextDouble();
+                                    System.out.println("result = " + calculator.resultSearch(n3));
+                                    break viewlabel;
+                                case "3":
+                                    System.out.println("result = " + viewList);
+                                    break viewlabel;
+                                case "4":
+                                    System.out.println("취소했습니다");
+                                    break viewlabel;
+                                default:
+                                    System.out.println("잘못된 입력입니다.");
+                                    break;
+                            }
+                        }
+                        break;
+                    // 삭제
+                    case "delete":
+                        System.out.println("삭제기능 목록");
+                        System.out.println("1. 모두 지우기");
+                        System.out.println("2. 이전값 지우기");
+                        System.out.println("3. 최근값 지우기");
+                        System.out.println("4. 취소");
+                        deletelabel:
+                        while (true) {
+                            System.out.print("원하시는 기능의 번호를 입력해주세요: ");
+                            delete = scanner.next();
+                            switch (delete) {
+                                case "1":
+                                    if (!viewList.isEmpty()) {
+                                        while (!viewList.isEmpty()) {
+                                            calculator.removeResultList();
+                                        }
+                                    }
+                                    System.out.println("삭제 완료");
+                                    break deletelabel;
+                                case "2":
+                                    if (!viewList.isEmpty()) {
+                                        calculator.removeResultList();
+                                        System.out.println("삭제 완료");
+                                    } else {
+                                        System.out.println("삭제할 값이 없습니다.");
+                                    }
+                                    break deletelabel;
+                                case "3":
+                                    if (!viewList.isEmpty()) {
+                                        calculator.removeResultList2();
+                                        System.out.println("삭제 완료");
+                                    } else {
+                                        System.out.println("삭제할 값이 없습니다.");
+                                    }
+                                    break deletelabel;
+                                case "4":
+                                    System.out.println("취소했습니다");
+                                    break deletelabel;
+                                default:
+                                    System.out.println("잘못된 입력입니다");
+                                    break;
+                            }
+                        }
+                        break;
+                    // 진행
+                    case "cont":
+                        break label;
+                    // 종료
+                    case "exit":
+                        replay = "exit";
+                        break label;
+                    // 해당되지 않는 값
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+                        break;
+                }
+            }
 
         } while (!replay.equals("exit"));
 
         // 결과 값 조회하기
-        System.out.print("값 조회하기(입력한 수보다 큰 수): ");
-        double n3 = scanner.nextDouble();
-        List<Double> listSearch = calculator.resultSearch(n3);
-        if (!listSearch.isEmpty()) {
-            System.out.println("조회된 값 = " + listSearch);
-        } else {
-            System.out.println("조회된 값이 없습니다.");
-        }
+//        System.out.print("값 조회하기(입력한 수보다 큰 수): ");
+//        double n3 = scanner.nextDouble();
+//        List<Double> listSearch = calculator.resultSearch(n3);
+//        if (!listSearch.isEmpty()) {
+//            System.out.println("조회된 값 = " + listSearch);
+//        } else {
+//            System.out.println("조회된 값이 없습니다.");
+//        }
 
         // 계산기 종료
         System.out.print("계산기 종료");
