9f7f50852f25810cf62b6ac795725148ea32a378
donghyeon505 <ehdgus458@gmail.com>
2025-07-14 10:34:30 +0900

    feat: 계산기 기능 분리로 가독성 향상 - 한 메서드에 기능이 몰려 있어 가독성이 떨어졌음 - view, delete 기능을 별도 메서드로 분리

--- src/main/java/lv3/CalculatorUI.java
+++ src/main/java/lv3/CalculatorUI.java
@@ -14,13 +14,91 @@ public class CalculatorUI {
         this.calculator = calculator;
     }
 
+    // view 메서드 분리
+    private void view() {
+        List<Double> viewList = calculator.getResultList();
+        String view;
+        double n3;
+
+        System.out.println("조회기능 목록");
+        System.out.println("1. 입력값보다 낮은 수");
+        System.out.println("2. 입력값보다 높은 수");
+        System.out.println("3. 전체 조회");
+        System.out.println("4. 취소");
+
+        while (true) {
+            System.out.print("조회 기능 번호 입력: ");
+            view = scanner.next();
+            switch (view) {
+                case "1":
+                    System.out.print("입력값: ");
+                    n3 = scanner.nextDouble();
+                    System.out.println("결과 = " + calculator.resultSearch2(n3));
+                    return;
+                case "2":
+                    System.out.print("입력값: ");
+                    n3 = scanner.nextDouble();
+                    System.out.println("결과 = " + calculator.resultSearch(n3));
+                    return;
+                case "3":
+                    System.out.println("전체 결과 = " + viewList);
+                    return;
+                case "4":
+                    System.out.println("취소");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+
+    // delete 메서드 분리
+    private void delete() {
+        List<Double> viewList = calculator.getResultList();
+        String delete;
+
+
+        System.out.println("삭제기능 목록");
+        System.out.println("1. 모두 지우기");
+        System.out.println("2. 이전값 지우기");
+        System.out.println("3. 최근값 지우기");
+        System.out.println("4. 취소");
+        while (true) {
+            System.out.print("삭제 기능 번호 입력: ");
+            delete = scanner.next();
+            switch (delete) {
+                case "1":
+                    viewList.clear(); // 전체 삭제
+                    System.out.println("전체 삭제 완료");
+                    return;
+                case "2":
+                    if (!viewList.isEmpty()) {
+                        calculator.removeResultList(); // 처음 값 삭제
+                        System.out.println("이전값 삭제 완료");
+                    } else {
+                        System.out.println("삭제할 값이 없습니다.");
+                    }
+                    return;
+                case "3":
+                    if (!viewList.isEmpty()) {
+                        calculator.removeResultList2(); // 마지막 값 삭제
+                        System.out.println("최근값 삭제 완료");
+                    } else {
+                        System.out.println("삭제할 값이 없습니다.");
+                    }
+                    return;
+                case "4":
+                    System.out.println("취소");
+                    return;
+                default:
+                    System.out.println("잘못된 입력입니다.");
+            }
+        }
+    }
+
     // 계산기 기능 메서드
     public String display() {
-        List<Double> viewList = calculator.getResultList();
         String replay;
-        String view;
-        String delete;
-        double n3;
 
         System.out.println("1. 결과값 조회하기");
         System.out.println("2. 결과값 초기화하기");
@@ -33,72 +111,11 @@ public class CalculatorUI {
 
             switch (replay) {
                 case "1":
-                    System.out.println("조회기능 목록");
-                    System.out.println("1. 입력값보다 낮은 수");
-                    System.out.println("2. 입력값보다 높은 수");
-                    System.out.println("3. 전체 조회");
-                    System.out.println("4. 취소");
-                    while (true) {
-                        System.out.print("조회 기능 번호 입력: ");
-                        view = scanner.next();
-                        switch (view) {
-                            case "1":
-                                System.out.print("입력값: ");
-                                n3 = scanner.nextDouble();
-                                System.out.println("결과 = " + calculator.resultSearch2(n3));
-                                return replay;
-                            case "2":
-                                System.out.print("입력값: ");
-                                n3 = scanner.nextDouble();
-                                System.out.println("결과 = " + calculator.resultSearch(n3));
-                                return replay;
-                            case "3":
-                                System.out.println("전체 결과 = " + viewList);
-                                return replay;
-                            case "4":
-                                System.out.println("취소");
-                                return replay;
-                            default:
-                                System.out.println("잘못된 입력입니다.");
-                        }
-                    }
+                    view();
+                    break;
                 case "2":
-                    System.out.println("삭제기능 목록");
-                    System.out.println("1. 모두 지우기");
-                    System.out.println("2. 이전값 지우기");
-                    System.out.println("3. 최근값 지우기");
-                    System.out.println("4. 취소");
-                    while (true) {
-                        System.out.print("삭제 기능 번호 입력: ");
-                        delete = scanner.next();
-                        switch (delete) {
-                            case "1":
-                                viewList.clear(); // 전체 삭제
-                                System.out.println("전체 삭제 완료");
-                                return replay;
-                            case "2":
-                                if (!viewList.isEmpty()) {
-                                    calculator.removeResultList(); // 처음 값 삭제
-                                    System.out.println("이전값 삭제 완료");
-                                } else {
-                                    System.out.println("삭제할 값이 없습니다.");
-                                }
-                                return replay;
-                            case "3":
-                                if (!viewList.isEmpty()) {
-                                    calculator.removeResultList2(); // 마지막 값 삭제
-                                    System.out.println("최근값 삭제 완료");
-                                } else {
-                                    System.out.println("삭제할 값이 없습니다.");
-                                }
-                                return replay;
-                            case "4":
-                                System.out.println("취소");
-                                return replay;
-                            default:
-                                System.out.println("잘못된 입력입니다.");
-                        }
-                    }
+                    delete();
+                    break;
                 case "3":
                     return replay;
                 case "exit":
